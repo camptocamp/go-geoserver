cbe4b5575749364d3a2a180618be6b6c33b1ed71
AlexGacon <alexandre.gacon@gmail.com>
2023-11-15 06:32:08 +0100

    Add WmsStore as resource

---
+++ client/wmsstore.go
@@ -0,0 +1,174 @@
+package client
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+)
+
+// WmsStoreReference is a reference to a WmsStore
+type WmsStoreReference struct {
+	Name string `xml:"name"`
+}
+
+// WmsStores is a list of WmsStoreReference reference
+type WmsStores struct {
+	XMLName xml.Name             `xml:"wmsStores"`
+	List    []*WmsStoreReference `xml:"wmsStore"`
+}
+
+// WmsStore is a Geoserver object
+type WmsStore struct {
+	XMLName                    xml.Name            `xml:"wmsStore"`
+	Name                       string              `xml:"name"`
+	Description                string              `xml:"description"`
+	Enabled                    bool                `xml:"enabled"`
+	Workspace                  *WorkspaceReference `xml:"workspace"`
+	Default                    bool                `xml:"__default"`
+	DisableConnectionOnFailure bool                `xml:"disableOnConnFailure"`
+	CapabilitiesUrl            string              `xml:"capabilitiesURL"`
+	MaxConnections             int                 `xml:"maxConnections"`
+	ReadTimeOut                int                 `xml:"readTimeout"`
+	ConnectTimeOut             int                 `xml:"connectTimeout"`
+}
+
+// GetWmsStores returns the list of the wms stores
+func (c *Client) GetWmsStores(workspace string) (wmsStores []*WmsStore, err error) {
+	statusCode, body, err := c.doRequest("GET", fmt.Sprintf("/workspaces/%s/wmsstores", workspace), nil)
+	if err != nil {
+		return
+	}
+
+	switch statusCode {
+	case 401:
+		err = fmt.Errorf("unauthorized")
+		return
+	case 200:
+		break
+	default:
+		err = fmt.Errorf("unknown error: %d - %s", statusCode, body)
+		return
+	}
+
+	var data WmsStores
+	if err := xml.Unmarshal([]byte(body), &data); err != nil {
+		return wmsStores, nil
+	}
+
+	for _, wmsStoreRef := range data.List {
+		wmsStore, err := c.GetWmsStore(workspace, wmsStoreRef.Name)
+		if err != nil {
+			return wmsStores, err
+		}
+
+		wmsStores = append(wmsStores, wmsStore)
+	}
+
+	return
+}
+
+// GetWmsStore return a single wms store based on its name
+func (c *Client) GetWmsStore(workspace, name string) (wmsStore *WmsStore, err error) {
+	statusCode, body, err := c.doRequest("GET", fmt.Sprintf("/workspaces/%s/wmsstores/%s", workspace, name), nil)
+	if err != nil {
+		return
+	}
+
+	switch statusCode {
+	case 401:
+		err = fmt.Errorf("unauthorized")
+		return
+	case 404:
+		err = fmt.Errorf("not found")
+		return
+	case 200:
+		break
+	default:
+		err = fmt.Errorf("unknown error: %d - %s", statusCode, body)
+		return
+	}
+
+	var data WmsStore
+	if err := xml.Unmarshal([]byte(body), &data); err != nil {
+		return wmsStore, err
+	}
+
+	wmsStore = &data
+
+	return
+}
+
+// CreateWmStore creates a wms store
+func (c *Client) CreateWmStore(workspace string, wmsStore *WmsStore) (err error) {
+	payload, _ := xml.Marshal(&wmsStore)
+	statusCode, body, err := c.doRequest("POST", fmt.Sprintf("/workspaces/%s/wmsstores", workspace), bytes.NewBuffer(payload))
+	if err != nil {
+		return
+	}
+
+	switch statusCode {
+	case 401:
+		err = fmt.Errorf("unauthorized")
+		return
+	case 201:
+		return
+	default:
+		err = fmt.Errorf("unknown error: %d - %s", statusCode, body)
+		return
+	}
+}
+
+// UpdateWmsStore updates a wms store
+func (c *Client) UpdateWmsStore(workspaceName, wmsStoreName string, wmsStore *WmsStore) (err error) {
+	payload, _ := xml.Marshal(&wmsStore)
+
+	statusCode, body, err := c.doRequest("PUT", fmt.Sprintf("/workspaces/%s/wmsstores/%s", workspaceName, wmsStoreName), bytes.NewBuffer(payload))
+	if err != nil {
+		return
+	}
+
+	switch statusCode {
+	case 401:
+		err = fmt.Errorf("unauthorized")
+		return
+	case 404:
+		err = fmt.Errorf("not found")
+		return
+	case 405:
+		err = fmt.Errorf("forbidden")
+		return
+	case 200:
+		return
+	default:
+		err = fmt.Errorf("unknown error: %d - %s", statusCode, body)
+		return
+	}
+}
+
+// DeleteWmsStore deletes a wms store
+func (c *Client) DeleteWmsStore(workspaceName, wmsStoreName string, recurse bool) (err error) {
+	statusCode, body, err := c.doRequest("DELETE", fmt.Sprintf("/workspaces/%s/wmsstores/%s?recurse=%t", workspaceName, wmsStoreName, recurse), nil)
+	if err != nil {
+		return
+	}
+
+	switch statusCode {
+	case 401:
+		err = fmt.Errorf("unauthorized")
+		return
+	case 403:
+		err = fmt.Errorf("workspace is not empty")
+		return
+	case 404:
+		err = fmt.Errorf("not found")
+		return
+	case 405:
+		err = fmt.Errorf("forbidden")
+		return
+	case 200:
+		return
+	default:
+		err = fmt.Errorf("unknown error: %d - %s", statusCode, body)
+		return
+	}
+}
---
+++ client/wmsstore_test.go
@@ -0,0 +1,354 @@
+package client
+
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetWmsStoresSuccess(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/workspaces/foo/wmsstores", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET")
+
+		w.WriteHeader(200)
+		w.Write([]byte(`
+		<wmsStores>
+			<wmsStore>
+				<name>sf</name>
+				<atom:link xmlns:atom="http://www.w3.org/2005/Atom" rel="alternate" href="http://localhost:8080/geoserver/rest/workspaces/sf/wmsstores/sf.xml" type="application/xml"/>
+			</wmsStore>
+		</wmsStores>
+		`))
+	})
+	mux.HandleFunc("/workspaces/foo/wmsstores/sf", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET")
+
+		w.WriteHeader(200)
+		w.Write([]byte(`
+		<wmsStore>
+		<name>sf</name>
+		<description>Services de la GeoPlateforme IGN</description>
+		<enabled>true</enabled>
+		<workspace>
+		  <name>foo</name>
+		  <atom:link 
+			xmlns:atom="http://www.w3.org/2005/Atom" rel="alternate" href="https://master.dev.scw.ansc.fr/geoserver/rest/workspaces/ign.xml" type="application/xml"/>
+		  </workspace>
+		  <__default>false</__default>
+		  <disableOnConnFailure>false</disableOnConnFailure>
+		  <capabilitiesURL>https://data.geopf.fr/wms-r/wms?SERVICE=WMS&amp;Version=1.3.0&amp;Request=GetCapabilities</capabilitiesURL>
+		  <maxConnections>6</maxConnections>
+		  <readTimeout>60</readTimeout>
+		  <connectTimeout>30</connectTimeout>
+		  <wmslayers>
+			<atom:link 
+			  xmlns:atom="http://www.w3.org/2005/Atom" rel="alternate" href="https://master.dev.scw.ansc.fr/geoserver/rest/workspaces/ign/wmsstores/GeoPlatforme/wmslayers.xml" type="application/xml"/>
+			</wmslayers>
+		  </wmsStore>
+		`))
+	})
+
+	testServer := httptest.NewServer(mux)
+	defer testServer.Close()
+
+	expectedResult := []*WmsStore{
+		{
+			XMLName: xml.Name{
+				Local: "wmsStore",
+			},
+			Name:    "sf",
+			Enabled: true,
+			Workspace: &WorkspaceReference{
+				Name: "foo",
+			},
+			Default:                    false,
+			Description:                "Services de la GeoPlateforme IGN",
+			DisableConnectionOnFailure: false,
+			CapabilitiesUrl:            "https://data.geopf.fr/wms-r/wms?SERVICE=WMS&Version=1.3.0&Request=GetCapabilities",
+			MaxConnections:             6,
+			ReadTimeOut:                60,
+			ConnectTimeOut:             30,
+		},
+	}
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	datastores, err := cli.GetWmsStores("foo")
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedResult, datastores)
+}
+
+func TestGetWmsStoreSuccess(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/workspaces/foo/wmsstores/sf", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET")
+
+		w.WriteHeader(200)
+		w.Write([]byte(`
+		<wmsStore>
+		<name>sf</name>
+		<description>Services de la GeoPlateforme IGN</description>
+		<enabled>true</enabled>
+		<workspace>
+		  <name>foo</name>
+		  <atom:link 
+			xmlns:atom="http://www.w3.org/2005/Atom" rel="alternate" href="https://master.dev.scw.ansc.fr/geoserver/rest/workspaces/ign.xml" type="application/xml"/>
+		  </workspace>
+		  <__default>false</__default>
+		  <disableOnConnFailure>false</disableOnConnFailure>
+		  <capabilitiesURL>https://data.geopf.fr/wms-r/wms?SERVICE=WMS&amp;Version=1.3.0&amp;Request=GetCapabilities</capabilitiesURL>
+		  <maxConnections>6</maxConnections>
+		  <readTimeout>60</readTimeout>
+		  <connectTimeout>30</connectTimeout>
+		  <wmslayers>
+			<atom:link 
+			  xmlns:atom="http://www.w3.org/2005/Atom" rel="alternate" href="https://master.dev.scw.ansc.fr/geoserver/rest/workspaces/ign/wmsstores/GeoPlatforme/wmslayers.xml" type="application/xml"/>
+			</wmslayers>
+		  </wmsStore>
+		`))
+	})
+
+	testServer := httptest.NewServer(mux)
+	defer testServer.Close()
+
+	expectedResult := &WmsStore{
+		XMLName: xml.Name{
+			Local: "wmsStore",
+		},
+		Name:    "sf",
+		Enabled: true,
+		Workspace: &WorkspaceReference{
+			Name: "foo",
+		},
+		Default:                    false,
+		Description:                "Services de la GeoPlateforme IGN",
+		DisableConnectionOnFailure: false,
+		CapabilitiesUrl:            "https://data.geopf.fr/wms-r/wms?SERVICE=WMS&Version=1.3.0&Request=GetCapabilities",
+		MaxConnections:             6,
+		ReadTimeOut:                60,
+		ConnectTimeOut:             30,
+	}
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	wmsStore, err := cli.GetWmsStore("foo", "sf")
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedResult, wmsStore)
+}
+
+func TestGetWmsStoreUnauthorized(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET")
+		assert.Equal(t, r.URL.Path, "/workspaces/foo/wmsstores/sf")
+
+		w.WriteHeader(401)
+		w.Write([]byte(``))
+	}))
+	defer testServer.Close()
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	wmsStore, err := cli.GetWmsStore("foo", "sf")
+
+	assert.Error(t, err, "Unauthorized")
+	assert.Nil(t, wmsStore)
+}
+
+func TestGetWmsStoreNotFound(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET")
+		assert.Equal(t, r.URL.Path, "/workspaces/foo/wmsstores/sf")
+
+		w.WriteHeader(404)
+		w.Write([]byte(``))
+	}))
+	defer testServer.Close()
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	wmsStore, err := cli.GetWmsStore("foo", "sf")
+
+	assert.Error(t, err, "Not Found")
+	assert.Nil(t, wmsStore)
+}
+
+func TestGetWmsStoreUnknownError(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET")
+		assert.Equal(t, r.URL.Path, "/workspaces/foo/wmsstores/sf")
+
+		w.WriteHeader(418)
+		w.Write([]byte(`I'm a teapot!`))
+	}))
+	defer testServer.Close()
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	wmsStore, err := cli.GetWmsStore("foo", "sf")
+
+	assert.Error(t, err, "Unknown error: 418 - I'm a teapot!")
+	assert.Nil(t, wmsStore)
+}
+
+func TestCreateWmsStoreSuccess(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "POST")
+		assert.Equal(t, r.URL.Path, "/workspaces/foo/wmsstores")
+
+		rawBody, err := ioutil.ReadAll(r.Body)
+		assert.Nil(t, err)
+		var payload *WmsStore
+		err = xml.Unmarshal(rawBody, &payload)
+		assert.Nil(t, err)
+		assert.Equal(t, payload, &WmsStore{
+			XMLName: xml.Name{
+				Local: "wmsStore",
+			},
+			Name:    "sf",
+			Enabled: true,
+			Workspace: &WorkspaceReference{
+				Name: "foo",
+			},
+			Default:                    false,
+			Description:                "Services de la GeoPlateforme IGN",
+			DisableConnectionOnFailure: false,
+			CapabilitiesUrl:            "https://data.geopf.fr/wms-r/wms?SERVICE=WMS&Version=1.3.0&Request=GetCapabilities",
+			MaxConnections:             6,
+			ReadTimeOut:                60,
+			ConnectTimeOut:             30,
+		})
+
+		w.WriteHeader(201)
+		w.Write([]byte(``))
+	}))
+	defer testServer.Close()
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	err := cli.CreateWmStore("foo", &WmsStore{
+		XMLName: xml.Name{
+			Local: "wmsStore",
+		},
+		Name:    "sf",
+		Enabled: true,
+		Workspace: &WorkspaceReference{
+			Name: "foo",
+		},
+		Default:                    false,
+		Description:                "Services de la GeoPlateforme IGN",
+		DisableConnectionOnFailure: false,
+		CapabilitiesUrl:            "https://data.geopf.fr/wms-r/wms?SERVICE=WMS&Version=1.3.0&Request=GetCapabilities",
+		MaxConnections:             6,
+		ReadTimeOut:                60,
+		ConnectTimeOut:             30,
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestUpdateWmsStoreSuccess(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "PUT")
+		assert.Equal(t, r.URL.Path, "/workspaces/foo/wmsstores/sf")
+
+		rawBody, err := ioutil.ReadAll(r.Body)
+		assert.Nil(t, err)
+		var payload *WmsStore
+		err = xml.Unmarshal(rawBody, &payload)
+		assert.Nil(t, err)
+		assert.Equal(t, payload, &WmsStore{
+			XMLName: xml.Name{
+				Local: "wmsStore",
+			},
+			Name:    "sf",
+			Enabled: true,
+			Workspace: &WorkspaceReference{
+				Name: "foo",
+			},
+			Default:                    false,
+			Description:                "Services de la GeoPlateforme IGN",
+			DisableConnectionOnFailure: false,
+			CapabilitiesUrl:            "https://data.geopf.fr/wms-r/wms?SERVICE=WMS&Version=1.3.0&Request=GetCapabilities",
+			MaxConnections:             6,
+			ReadTimeOut:                60,
+			ConnectTimeOut:             30,
+		})
+
+		w.WriteHeader(200)
+		w.Write([]byte(``))
+	}))
+	defer testServer.Close()
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	err := cli.UpdateWmsStore("foo", "sf", &WmsStore{
+		XMLName: xml.Name{
+			Local: "wmsStore",
+		},
+		Name:    "sf",
+		Enabled: true,
+		Workspace: &WorkspaceReference{
+			Name: "foo",
+		},
+		Default:                    false,
+		Description:                "Services de la GeoPlateforme IGN",
+		DisableConnectionOnFailure: false,
+		CapabilitiesUrl:            "https://data.geopf.fr/wms-r/wms?SERVICE=WMS&Version=1.3.0&Request=GetCapabilities",
+		MaxConnections:             6,
+		ReadTimeOut:                60,
+		ConnectTimeOut:             30,
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestDeleteWmsStoreSuccess(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "DELETE")
+		assert.Equal(t, r.URL.Path, "/workspaces/foo/wmsstores/sf")
+		keys, ok := r.URL.Query()["recurse"]
+		assert.True(t, ok)
+		assert.Equal(t, keys[0], "true")
+
+		w.WriteHeader(200)
+		w.Write([]byte(``))
+	}))
+	defer testServer.Close()
+
+	cli := &Client{
+		URL:        testServer.URL,
+		HTTPClient: &http.Client{},
+	}
+
+	err := cli.DeleteWmsStore("foo", "sf", true)
+
+	assert.Nil(t, err)
+}
